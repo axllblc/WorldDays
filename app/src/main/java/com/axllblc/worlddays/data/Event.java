b1c91f92b3e4c3d8090f5f616d48528deeebdd37
Axel Leblanc <axel.leblanc@etu.univ-tours.fr>
2024-03-29 23:26:55 +0100

    Changes in Event class
    * Migrated java.util.Calendar to java.time.LocalDate
    * Replaced month (int) and dayOfMonth (int) by monthDay (java.time.MonthDay)
    * Removed organizer and officialWebsite
    * toString (with Lombok)

--- app/src/main/java/com/axllblc/worlddays/data/Event.java
+++ app/src/main/java/com/axllblc/worlddays/data/Event.java
@@ -1,11 +1,13 @@
 package com.axllblc.worlddays.data;
 
-
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.Year;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 import lombok.With;
 
 /**
@@ -14,6 +16,7 @@ import lombok.With;
 @AllArgsConstructor
 @Getter
 @With
+@ToString
 public class Event {
     // 👇 Basic information about the event
     /**
@@ -25,133 +28,106 @@ public class Event {
      */
     private final @NonNull String title;
     /**
-     * Month, between 1 and 12 (included).
+     * Day in year for periodic occurrence.
      */
-    private final int month;
-    /**
-     * Day of month, between 1 and 31.
-     */
-    private final int dayOfMonth;
+    private final @NonNull MonthDay monthDay;
     // 👇 Details about the event
     /**
      * URL of the Wikipedia article related to this event.
+     * [optional]
      */
     private final String wikipediaURL;
     /**
      * Introduction of the Wikipedia article related to this event.
+     * [optional]
      */
     private final String wikipediaIntro;
     /**
      * Date of creation of this event.
+     * [optional]
      */
-    private final String inception;
-    /**
-     * Organizer of this event.
-     */
-    private final String organizer;
+    private final LocalDate inception;
     /**
      * Founder of this event.
+     * [optional]
      */
     private final String founder;
-    /**
-     * URL of the official website for this event.
-     */
-    private final String officialWebsite;
 
     private final boolean isDetailed;
 
-    public Event(@NonNull String id, @NonNull String title, int month, int dayOfMonth) {
+    public Event(@NonNull String id, @NonNull String title, @NonNull MonthDay monthDay) {
         this.id = id;
         this.title = title;
-        this.month = month;
-        this.dayOfMonth = dayOfMonth;
+        this.monthDay = monthDay;
 
         this.wikipediaURL = null;
         this.wikipediaIntro = null;
         this.inception = null;
-        this.organizer = null;
         this.founder = null;
-        this.officialWebsite = null;
         this.isDetailed = false;
     }
 
     public Event(@NonNull String id,
                  @NonNull String title,
-                 int month,
-                 int dayOfMonth,
+                 @NonNull MonthDay monthDay,
                  String wikipediaURL,
                  String wikipediaIntro,
-                 String inception,
-                 String organizer,
-                 String founder,
-                 String officialWebsite
+                 LocalDate inception,
+                 String founder
     ) {
         this.id = id;
         this.title = title;
-        this.month = month;
-        this.dayOfMonth = dayOfMonth;
+        this.monthDay = monthDay;
         this.wikipediaURL = wikipediaURL;
         this.wikipediaIntro = wikipediaIntro;
         this.inception = inception;
-        this.organizer = organizer;
         this.founder = founder;
-        this.officialWebsite = officialWebsite;
         this.isDetailed = true;
     }
 
     /**
-     * Returns a {@link Calendar} object containing the date of the last occurrence of this event,
+     * Returns a {@link LocalDate} object containing the date of the last occurrence of this event,
      * based on current time.
+     *
      * @return Date of the last occurrence of this event
      */
-    public Calendar getLastOccurrence() {
-        return getOccurrence(false);
-    }
-
-    /**
-     * Returns a {@link Calendar} object containing the date of the next occurrence of this event,
-     * based on current time.
-     * @return Date of the next occurrence of this event
-     */
-    public Calendar getNextOccurrence() {
-        return getOccurrence(true);
-    }
-
-    /**
-     * Returns a {@link Calendar} object containing the date of the occurrence of this event for
-     * given year.
-     * @return Date of the occurrence of this event for given year
-     */
-    public Calendar getOccurrenceForYear(int year) {
-        return new Calendar.Builder()
-                .set(Calendar.YEAR, year)
-                .set(Calendar.MONTH, month)
-                .set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                .build();
-    }
-
-    /**
-     * Returns a {@link Calendar} object containing the date of the next occurrence of this event,
-     * if {@code next} is true, the last occurrence otherwise.
-     * @return Date of the last/next occurrence of this event
-     */
-    private Calendar getOccurrence(boolean next) {
-        Calendar now = Calendar.getInstance();
-        int currentDayOfMonth = now.get(Calendar.DAY_OF_MONTH);
-        int currentMonth = now.get(Calendar.MONTH) + 1;
-        int year = now.get(Calendar.YEAR);
-
-        if (month < currentMonth || (month == currentMonth && dayOfMonth < currentDayOfMonth))
+    public LocalDate getLastOccurrence() {
+        LocalDate thisYearOccurrence = monthDay.atYear(Year.now().getValue());
+        if (thisYearOccurrence.isBefore(LocalDate.now()))
             // The occurrence for this year is in the past
             // Last occurrence → this year
-            // Next occurrence → next year
-            year = next ? year + 1 : year;
+            return thisYearOccurrence;
         else
             // The occurrence for this year is in the future
             // Last occurrence → last year
-            // Next occurrence → this year
-            year = next ? year - 1 : year;
+            return thisYearOccurrence.minusYears(1);
+    }
 
-        return getOccurrenceForYear(year);
+    /**
+     * Returns a {@link LocalDate} object containing the date of the next occurrence of this event,
+     * based on current time.
+     *
+     * @return Date of the next occurrence of this event
+     */
+    public LocalDate getNextOccurrence() {
+        LocalDate thisYearOccurrence = monthDay.atYear(Year.now().getValue());
+        if (thisYearOccurrence.isBefore(LocalDate.now()))
+            // The occurrence for this year is in the past
+            // Next occurrence → next year
+            return thisYearOccurrence.plusYears(1);
+        else
+            // The occurrence for this year is in the future
+            // Next occurrence → this year
+            return thisYearOccurrence;
+    }
+
+    /**
+     * Returns a {@link LocalDate} object containing the date of the occurrence of this event for
+     * given year.
+     *
+     * @return Date of the occurrence of this event for given year
+     */
+    public LocalDate getOccurrenceForYear(int year) {
+        return monthDay.atYear(year);
     }
 }
